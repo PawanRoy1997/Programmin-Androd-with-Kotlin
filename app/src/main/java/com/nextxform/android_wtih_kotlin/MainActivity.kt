fd65054c297a75809e0bf42d938715555125e122
Pawan <pawanroy0066@gmail.com>
2023-10-22 11:17:17 +0530

    Initial Bare Project

---
+++ app/src/main/java/com/nextxform/android_wtih_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nextxform.android_wtih_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
